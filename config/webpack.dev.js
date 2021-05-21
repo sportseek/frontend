b94390e7cf93e2ef305ff4a74b589f7e02d3a39d
Md Muftee <mahfuzul.muftee@tum.de>
2021-05-21 23:55:58 +0200

    chore: add webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require("path")
+const { merge } = require("webpack-merge")
+const common = require("./webpack.common.js")
+
+module.exports = merge(common, {
+  devtool: "inline-source-map",
+  mode: "development",
+  target: "web",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true,
+    writeToDisk: true,
+  },
+})
