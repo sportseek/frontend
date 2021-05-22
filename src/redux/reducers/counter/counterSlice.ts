eb10d90aff578e51742a67d4923080e3d6d4a153
Md Muftee <mahfuzul.muftee@tum.de>
2021-05-22 02:28:52 +0200

    fix : redux

--- src/redux/reducers/counter/counterSlice.ts
+++ src/redux/reducers/counter/counterSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { fetchCount } from "./counterAPI"
+import fetchCount from "./counterAPI"
 
 export interface CounterState {
   value: number
