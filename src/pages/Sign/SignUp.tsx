66f6212a7bf4c64829d63b40d2447ea1f42ee53c
A.B.M. Shahriar <ge79vux@mytum.de>
2021-05-25 02:11:12 +0200

    chore: implement player signup

--- src/pages/Sign/SignUp.tsx
+++ src/pages/Sign/SignUp.tsx
@@ -1,5 +1,19 @@
 import React, { FC } from "react"
+import { Redirect } from "react-router-dom"
+import { useAppSelector } from "redux/hooks"
+import { RootState } from "redux/store"
+import PlayerSignup from "../../components/PlayerSignup"
 
-const SignUpPage: FC = () => <div />
+const SignUpPage: FC = () => {
+  const isAuthenticated = useAppSelector(
+    (state: RootState) => state.user.isAuthenticated
+  )
+  console.log(isAuthenticated)
+  return isAuthenticated ? (
+    <Redirect to={{ pathname: "/home" }} />
+  ) : (
+    <PlayerSignup />
+  )
+}
 
 export default SignUpPage
