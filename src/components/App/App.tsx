444250e9882eb66e0e6231e6cd216737d8cd3e8f
Md Muftee <mahfuzul.muftee@tum.de>
2021-05-26 19:26:33 +0200

    feat: add react scrollbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core"
 import { BrowserRouter as Router } from "react-router-dom"
 import { StylesProvider } from "@material-ui/styles"
-
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import 'react-perfect-scrollbar/dist/css/styles.css'
 import Routes from "routes"
 import theme from "theme"
 
 const App = () => (
   <>
+   <PerfectScrollbar>
     <StylesProvider injectFirst>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
@@ -16,6 +18,7 @@ const App = () => (
         </Router>
       </MuiThemeProvider>
     </StylesProvider>
+    </PerfectScrollbar>
   </>
 )
 
