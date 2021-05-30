677f130bc94ceb080a21d766ac37df3753f18569
Md Muftee <mahfuzul.muftee@tum.de>
2021-05-30 11:07:58 +0200

    feat: add auth reducer

--- src/redux/reducers/auth/authSlice.ts
+++ src/redux/reducers/auth/authSlice.ts
@@ -1,31 +1,73 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { RootState } from "redux/store"
+import authAPI from "./authAPI"
+
+type AuthStatus = "idle" | "loggedIn" | "requesting" | "failed"
 
 interface AuthState {
   isAuthenticated: boolean
   type: "player" | "arena"
   userid: string
+  status: AuthStatus
+  errorMsg: string
 }
+
+export const userSignIn = createAsyncThunk("auth/signin", async () => {
+  const response = await authAPI.signup()
+  return response.data
+})
+
+export const userSignUp = createAsyncThunk("auth/signup", async () => {
+  const response = await authAPI.signup()
+  return response.data
+})
 
 const initialState: AuthState = {
   isAuthenticated: true,
   type: "player",
   userid: "12345",
+  status: "idle",
+  errorMsg: "",
 }
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: "user",
   initialState,
   reducers: {
-    signin: (state) => {
-      state.isAuthenticated = true
-    },
-    signup: (state) => {
-      state.isAuthenticated = true
-    },
     logout: (state) => {
       state.isAuthenticated = false
+      state.errorMsg = ""
+      state.status = "idle"
+      state.userid = ""
+      window.localStorage.removeItem("jwtToken")
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(userSignIn.fulfilled, (state, action) => {
+        state.isAuthenticated = action.payload
+        state.status = "loggedIn"
+      })
+      .addCase(userSignIn.pending, (state) => {
+        state.status = "requesting"
+      })
+      .addCase(userSignIn.rejected, (state, action) => {
+        state.isAuthenticated = false
+        state.status = "failed"
+        state.errorMsg = action.payload as string
+      })
+      .addCase(userSignUp.fulfilled, (state, action) => {
+        state.isAuthenticated = action.payload
+        state.status = "loggedIn"
+      })
+      .addCase(userSignUp.rejected, (state, action) => {
+        state.isAuthenticated = false
+        state.status = "failed"
+        state.errorMsg = action.payload as string
+      })
+      .addCase(userSignUp.pending, (state) => {
+        state.status = "requesting"
+      })
   },
 })
 
@@ -34,6 +76,6 @@ export const isIfAuthenticated = (state: RootState) =>
 export const selectUserType = (state: RootState) => state.auth.type
 export const selectUserId = (state: RootState) => state.auth.userid
 
-export const { signin, signup, logout } = authSlice.actions
+export const { logout } = authSlice.actions
 
 export default authSlice.reducer
