aea0f11030555b9aad36776f94ecf74f3837c3ef
qffdhruba <qffdhrubahome@gmail.com>
2021-07-06 21:35:48 +0200

    Location autocomplete

--- src/components/FilterEvents/FilterEvents.tsx
+++ src/components/FilterEvents/FilterEvents.tsx
@@ -8,13 +8,7 @@ import Grid from "@material-ui/core/Grid"
 import TextField from "@material-ui/core/TextField"
 import MenuItem from "@material-ui/core/MenuItem"
 import Slider from "@material-ui/core/Slider"
-import {
-  LocationOn,
-  SportsBasketball,
-  Euro,
-  People,
-  Search,
-} from "@material-ui/icons"
+import { LocationOn, SportsBasketball, Search } from "@material-ui/icons"
 import { Button } from "@material-ui/core"
 import { useAppDispatch, useAppSelector } from "redux/hooks"
 import {
@@ -26,7 +20,54 @@ import {
 } from "redux/reducers/event/eventSlice"
 import moment from "moment"
 
-const drawerWidth = 240
+import Autocomplete from "@material-ui/lab/Autocomplete"
+import parse from "autosuggest-highlight/parse"
+import throttle from "lodash/throttle"
+import Geocode from "react-geocode"
+
+Geocode.setApiKey("AIzaSyC3piWVpJ50bb8sVq-vGZnf6nbJMgyNtSE")
+Geocode.setLanguage("en")
+
+function latlngGen(place: PlaceType) {
+  var loc: any[]
+  Geocode.fromAddress(place.description).then(
+    (response) => {
+      loc[0] = response.results[0].geometry.location[0]
+      loc[1] = response.results[0].geometry.location[1]
+      console.log(loc);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+}
+
+function loadScript(src: string, position: HTMLElement | null, id: string) {
+  if (!position) {
+    return
+  }
+
+  const script = document.createElement("script")
+  script.setAttribute("async", "")
+  script.setAttribute("id", id)
+  script.src = src
+  position.appendChild(script)
+}
+
+const autocompleteService = { current: null }
+interface PlaceType {
+  description: string
+  structured_formatting: {
+    main_text: string
+    secondary_text: string
+    main_text_matched_substrings: [
+      {
+        offset: number
+        length: number
+      }
+    ]
+  }
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -86,29 +127,55 @@ const FilterEvents = () => {
   const classes = useStyles()
   const dispatch = useAppDispatch()
 
+  const [location, setLocation] = React.useState<PlaceType | null>(null)
+  const [inputLoc, setInputLoc] = React.useState("")
+  const [options, setOptions] = React.useState<PlaceType[]>([])
+  const loaded = React.useRef(false)
+
+  if (typeof window !== "undefined" && !loaded.current) {
+    if (!document.querySelector("#google-maps")) {
+      loadScript(
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyDF_-gtDuOK1_Y7T9HhOTuLJgOuo1yoM-w&libraries=places",
+        document.querySelector("head"),
+        "google-maps"
+      )
+    }
+
+    loaded.current = true
+  }
+
+  const fetch = React.useMemo(
+    () =>
+      throttle(
+        (
+          request: { input: string },
+          callback: (results?: PlaceType[]) => void
+        ) => {
+          ;(autocompleteService.current as any).getPlacePredictions(
+            request,
+            callback
+          )
+        },
+        200
+      ),
+    []
+  )
+
   const minPrice = useAppSelector(selectEventMinPrice)
   const maxPrice = useAppSelector(selectEventMaxPrice)
-
 
   let minDate = useAppSelector(selectEventMinDate)
   let maxDate = useAppSelector(selectEventMaxDate)
 
-  // minDate = moment(minDate).format("YYYY-MM-DDTHH:MM")
-  // maxDate = moment(maxDate).format("YYYY-MM-DDTHH:MM")
-
   const [eventTitle, setEventTitle] = useState("")
   const [sportsType, setSportsType] = useState("all")
-  
+
   const [eventStartTime, setEventStartTime] = useState(
     moment().format("YYYY-MM-DDTHH:MM")
   )
   const [eventEndTime, setEventEndTime] = useState(
     moment().format("YYYY-MM-DDTHH:MM")
   )
-
-  const handleChangeType = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSportsType(event.target.value)
-  }
 
   const [eventFee, setEventFee] = React.useState<number[]>([minPrice, maxPrice])
 
@@ -123,6 +190,44 @@ const FilterEvents = () => {
   useEffect(() => {
     setEventEndTime(moment(maxDate).format("YYYY-MM-DDTHH:MM"))
   }, [maxDate])
+
+  React.useEffect(() => {
+    let active = true
+
+    if (!autocompleteService.current && (window as any).google) {
+      autocompleteService.current = new (
+        window as any
+      ).google.maps.places.AutocompleteService()
+    }
+    if (!autocompleteService.current) {
+      return undefined
+    }
+
+    if (inputLoc === "") {
+      setOptions(location ? [location] : [])
+      return undefined
+    }
+
+    fetch({ input: inputLoc }, (results?: PlaceType[]) => {
+      if (active) {
+        let newOptions = [] as PlaceType[]
+
+        if (location) {
+          newOptions = [location]
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results]
+        }
+
+        setOptions(newOptions)
+      }
+    })
+
+    return () => {
+      active = false
+    }
+  }, [location, inputLoc, fetch])
 
   const handleChangePrice = (event: any, newValue: number | number[]) => {
     setEventFee(newValue as number[])
@@ -187,7 +292,65 @@ const FilterEvents = () => {
                 <Search />
               </Grid>
               <Grid item xs={10}>
-                <TextField label="Location" className={classes.textField} />
+                <Autocomplete
+                  style={{ width: 300 }}
+                  getOptionLabel={(option) =>
+                    typeof option === "string" ? option : option.description
+                  }
+                  filterOptions={(x) => x}
+                  options={options}
+                  autoComplete
+                  includeInputInList
+                  filterSelectedOptions
+                  value={location}
+                  onChange={(event: any, newValue: PlaceType | null) => {
+                    setOptions(newValue ? [newValue, ...options] : options)
+                    setLocation(newValue)
+                    newValue? console.log(latlngGen(newValue)) : console.log("NULL")
+                  }}
+                  onInputChange={(event, newInputValue) => {
+                    setInputLoc(newInputValue)
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Location"
+                      className={classes.textField}
+                    />
+                  )}
+                  renderOption={(option) => {
+                    const matches =
+                      option.structured_formatting.main_text_matched_substrings
+                    const parts = parse(
+                      option.structured_formatting.main_text,
+                      matches.map((match: any) => [
+                        match.offset,
+                        match.offset + match.length,
+                      ])
+                    )
+
+                    return (
+                      <Grid container alignItems="center">
+                        <Grid item>
+                          <LocationOn />
+                        </Grid>
+                        <Grid item xs>
+                          {parts.map((part: any, index: any) => (
+                            <span
+                              key={index}
+                              style={{ fontWeight: part.highlight ? 700 : 400 }}
+                            >
+                              {part.text}
+                            </span>
+                          ))}
+                          <Typography variant="body2" color="textSecondary">
+                            {option.structured_formatting.secondary_text}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={2}>
                 <LocationOn />
